9f67ebd111ec16e260ea9669c40fa914077a1868
lucas lacroix <lacroixlucas59@gmail.com>
2019-10-26 17:39:42 +0200

    ajout des titres de page en plus des icons sur le header

--- componant/header.php
+++ componant/header.php
@@ -2,8 +2,8 @@
         <div class="headerdis">
             <a href="index.php"><img src="img/logo.svg"  alt="logo"/></a>
             <span>
-                <a href="index.php"><img src="img/noter.svg" alt="note"/></a>
-                <a href="classement.php"><img src="img/classement.svg" alt="classement"/></a>
+                <a href="index.php"><img src="img/noter.svg" alt="note"/><div class="r">Voter</div></a>
+                <a href="classement.php"><img src="img/classement.svg" alt="classement"/><div class="r">Classement</div></a>
             </span>
         </div>
 </header>
