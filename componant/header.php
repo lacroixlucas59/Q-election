022afe5543fea0817fae86fb281bb05a13f4ce93
lucas lacroix <lacroixlucas59@gmail.com>
2019-10-27 18:49:35 +0100

    ajout du mail pour envoie d'image

--- componant/header.php
+++ componant/header.php
@@ -4,6 +4,7 @@
             <span>
                 <a href="index.php"><img src="img/noter.svg" alt="note"/><div class="r">Voter</div></a>
                 <a href="classement.php"><img src="img/classement.svg" alt="classement"/><div class="r">Classement</div></a>
+                <a href="mailto:tulasvumonq@sitedeq.fr"><img src="img/mail.svg" alt="mail"/><div class="r">Envoyer</div></a>
             </span>
         </div>
 </header>
